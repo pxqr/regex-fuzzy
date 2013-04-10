c853c39822d36b9443055f83f2b26c33a4bcef49
Sam T <sta.cs.vsu@gmail.com>
2013-04-10 20:27:24 +0400

    ~ perf fix

--- src/Text/Regex/Fuzzy/Gen.hs
+++ src/Text/Regex/Fuzzy/Gen.hs
@@ -4,11 +4,48 @@ module Text.Regex.Fuzzy.Gen
 
 import Control.Applicative
 import Control.Monad
+import Data.Array
 import Data.Attoparsec.Text
 import           Data.Text (Text)
 import qualified Data.Text as T
-import Text.Regex.Fuzzy.AST
 
+import Text.Regex.Fuzzy.AST
+import Text.Regex.Fuzzy.Simpl
+
+inItems :: [Item] -> Char -> Bool
+inItems xs c = any (inItem c) xs
+
+type Matcher = Parser Int
+
+mkSExp :: SExp -> Matcher
+mkSExp (SChar c) = char c >> return 1
+mkSExp (SText t) = string t >> return (T.length t)
+mkSExp (SCat xs) = go (snd (bounds xs))
+  where
+    go 0 = mkSExp (xs ! 0)
+    go n = do
+      i  <- mkSExp (xs ! n)
+      is <- go (pred n)
+      return (i + is)
+
+mkSExp (SAlt xs) = error "mkSExp" -- choice (map mkSExp xs)
+mkSExp  SAny     = anyChar >> return 1
+mkSExp (SPos xs) = satisfy (inItems xs) >> return 1
+mkSExp (SNeg _)  = error "mkSExp"
+mkSExp  SEOS     = endOfLine >> return 0
+mkSExp  SSOS     = error "mkSExp"
+mkSExp  SEmpty   = return 0
+
+newtype Regex = Regex Matcher
+
+mkRegex :: Exp -> Regex
+mkRegex = Regex . mkSExp . simpl
+
+tryMatch :: Regex -> Text -> Either String Int
+tryMatch (Regex p) t = parseOnly p t
+
+
+{-
 mkItem :: Item -> Parser ()
 mkItem (CharI c)   = void (char c)
 mkItem (Range a b) = void (satisfy (\x -> a <= x && x <= b))
@@ -16,13 +53,7 @@ mkItem (Range a b) = void (satisfy (\x -> a <= x && x <= b))
 mkClass :: Class -> Parser ()
 mkClass  AnyC        = void (anyChar)
 mkClass (PosSetC xx) = void (satisfy (inItems xx))
-  where
-    inItems :: [Item] -> Char -> Bool
-    inItems xs c = any (inItem c) xs
-
 mkClass (NegSetC _)  = error "mkClass"
-
-type Matcher = Parser Int
 
 mkAtom :: Atom -> Matcher
 mkAtom  SOS       = error "mkAtom"
@@ -44,11 +75,4 @@ mkExp (CatE xx)   = go xx
 
 mkExp (AlterE xs) = choice (map mkExp xs)
 {-# INLINE mkExp #-}
-
-newtype Regex = Regex Matcher
-
-mkRegex :: Exp -> Regex
-mkRegex = Regex . mkExp
-
-tryMatch :: Regex -> Text -> Either String Int
-tryMatch (Regex p) t = parseOnly p t
+-}
---
+++ src/Text/Regex/Fuzzy/Simpl.hs
@@ -0,0 +1,72 @@
+module Text.Regex.Fuzzy.Simpl
+       ( SExp(..)
+       , simpl
+       ) where
+
+import Control.Applicative
+import Data.Array
+--import Control.Monad.State
+import Data.Maybe
+import Data.Text (Text)
+import Text.Regex.Fuzzy.AST
+
+--type Fuel = State Int
+
+--withFuel :: Fuel a -> Int -> a
+--withFuel = evalState
+
+-- CPS?
+data SExp = SChar Char
+          | SText Text
+          | SCat  (Array Int SExp) -- TODO: array
+          | SAlt  (Array Int SExp)
+          | SAny
+          | SPos [Item]
+          | SNeg [Item]
+          | SEOS
+          | SSOS
+          | SEmpty
+            deriving Show
+
+simplClass :: Class -> SExp
+simplClass  AnyC       = SAny
+simplClass (PosSetC s) = SPos s
+simplClass (NegSetC s) = SNeg s
+
+simplAtom :: Atom -> SExp
+simplAtom  EOS       = SEOS
+simplAtom  SOS       = SSOS
+simplAtom (CharA  c) = SChar c
+simplAtom (ClassA c) = simplClass c
+
+simplMany :: [Exp] -> Array Int SExp
+simplMany xs = fmap simpl (listArray (0, length xs - 1) xs)
+
+simpl :: Exp -> SExp
+simpl  EmptyE     = SEmpty
+simpl (AtomE a)   = simplAtom a
+simpl (CatE xs)   = SCat (simplMany xs)
+simpl (AlterE xs) = SAlt (simplMany xs)
+
+{-
+simplify :: Exp -> Exp
+simplify _r = fromMaybe _r (go _r)
+  where
+    refine r = go r <|> Just r
+
+    goRec2 :: (Exp -> Exp -> Exp) -> Exp -> Exp -> Maybe Exp
+    goRec2 con r1 r2 =
+      case (go r1, go r2) of
+        (Nothing,  Nothing ) -> Nothing
+        (Just r1', Just r2') -> refine (con r1' r2')
+        (Just r1', Nothing ) -> refine (con r1' r2 )
+        (Nothing,  Just r2') -> refine (con r1  r2')
+
+    go :: Exp -> Maybe Exp
+    go (CatE xs) = Nothing
+    go r = return r
+    -- prefix tree
+
+    zipPrefix :: Exp -> Exp -> (Exp, (Exp, Exp))
+    zipPrefix = undefined
+-}
